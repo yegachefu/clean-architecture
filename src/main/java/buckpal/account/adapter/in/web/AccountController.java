b9b479054e20bfd9fe832979983eb3b4a3e3efe7
kwanguuuu <kwangyu326@gmail.com>
2022-01-13 22:17:37 +0900

    [CH5] feat: step1. 웹 어댑터 구현하기 예제코드 - 한개의 컨트롤러에 모두 다 때려넣기

--- src/main/java/buckpal/account/adapter/in/web/AccountController.java
+++ src/main/java/buckpal/account/adapter/in/web/AccountController.java
@@ -1,4 +1,40 @@
 package buckpal.account.adapter.in.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
 public class AccountController {
+
+	@GetMapping("/accounts")
+	List<AccountResource> listAccounts(){
+		// TODO: 2022/01/13
+	}
+
+	@GetMapping("/accounts/{accountId}")
+	AccountResource getAccount(@PathVariable("accountId") Long accountId) {
+		// TODO: 2022/01/13
+	}
+
+	@GetMapping("/accounts/{accountId}/balance")
+	AccountResource geAccountBalance(@PathVariable("accountId") Long accountId) {
+		// TODO: 2022/01/13
+	}
+	@PostMapping("/accounts")
+	AccountResource createAccount(@RequestBody AccountResource account) {
+		// TODO: 2022/01/13
+	}
+
+	@PostMapping("/accounts/send/{sourceAccountId}/{targetAccountId}/{amount}")
+	AccountResource createAccount(
+			@PathVariable("sourceAccountId") Long sourceAccountId,
+			@PathVariable("targetAccountId") Long targetAccountId,
+			@PathVariable("amount") Long amount
+	) {
+		// TODO: 2022/01/13
+	}
 }
